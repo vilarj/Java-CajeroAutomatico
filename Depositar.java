95759cf1cf3bf5d7dffa778a6dc20df00adbaa83
Vilarj <lamaravillasan@outlook.com>
2019-06-03 09:48:09 -0400

    Depositar.java
    Ahora esta funcionalidad funciona sin problemas. He dado algo de estetica tambien.

--- Depositar.java
+++ Depositar.java
@@ -4,24 +4,20 @@ public class Depositar extends Funcionalidades{
 
 	@Override
 	public void Transaccion() {
-		System.out.print("\nEste es su saldo actual: " + getSaldo());
-		
 		System.out.print("\n*********************************");
-		System.out.print("\nIntroduzca la cantidad del deposito: ");
+		System.out.print("\nIntroduzca la cantidad del deposito: $");
 		Deposito();
 		
 		System.out.print("\n*********************************");
 		if(deposito > 0) {
-			System.out.print("\n*********************************");
+			transaccion = getSaldo();
 			setSaldo(transaccion + deposito);
 			System.out.print("\n*********************************");
 			System.out.print("\nProcesando...");
-			System.out.print("\n*********************************");
-			System.out.print("\nDepositaste: " + getDeposito());
-			System.out.print("\n*********************************");
-			System.out.print("\nSu saldo actual es: " + getSaldo() + "\n");
+			System.out.print("\nSu saldo actual es: $" + getSaldo());
+			System.out.print("\n*********************************\n");
 			}
-		else {System.out.print("\nEl deposito debe ser mayor que 0");}
+		else {System.out.print("\nEl deposito debe ser mayor que 0\n");}
 	}
 	
 }
