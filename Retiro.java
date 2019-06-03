528632514b3512de970899435b1d8ac862c4e794
Vilarj <lamaravillasan@outlook.com>
2019-06-03 09:48:56 -0400

    Retiro.java
    He actualizado la estetica

--- Retiro.java
+++ Retiro.java
@@ -4,15 +4,14 @@ public class Retiro extends Funcionalidades{
 	
 	@Override
 	public void Transaccion() {
-		System.out.print("\nEste es su saldo disponible: " + getSaldo());
 		System.out.print("\n*********************************");
 		
-		System.out.print("\nIntroduzca el monto que desea retirar: " );
+		System.out.print("\nIntroduzca el monto que desea retirar: $" );
 		Retiro();
 		
 		if(retiro > getSaldo()) {
 			System.out.print("\n*********************************");
-			System.out.print("\n No puede retirar " + getRetiro() + ". La cantidad es mayor que su saldo.\n");
+			System.out.print("\n No puede retirar esta cantidad. La cantidad es mayor que su saldo.\n");
 		}
 		else if(retiro <= getSaldo()) {
 			transaccion = getSaldo();
@@ -20,12 +19,9 @@ public class Retiro extends Funcionalidades{
 			setSaldo(transaccion - retiro);
 			System.out.print("\n*********************************");
 			System.out.print("\nProcesando...");
-			System.out.print("\n*********************************");
-			System.out.print("\nAcaba de retirar: " + getRetiro());
-			System.out.print("\n*********************************");
-			System.out.print("\n...");
-			System.out.print("\n*********************************");
-			System.out.print("\nSu saldo actual es: " + getSaldo() + "\n");
+
+			System.out.print("\nSu saldo actual es: $" + getSaldo());
+			System.out.print("\n*********************************\n");
 		}
 		else {System.out.print("\n*********************************"); System.out.print("Esta accion es invalida.\n");}
 	}
